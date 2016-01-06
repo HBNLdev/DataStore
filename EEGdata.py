336e4c41315b572cb967cf8d0ac428f404bbd11d
mort <mort@mp4.localdomain>
2016-01-06 11:52:05 -0500

    Updates selected_cases_by_channel to take channel_list as an argument.  Also implements make_source_data for server version.

--- EEGdata.py
+++ EEGdata.py
@@ -5,7 +5,7 @@ import numpy as np
 import h5py, os
 import bokeh
 from bokeh.plotting import figure, output_notebook, show, gridplot
-from bokeh.models import FixedTicker, CustomJS, TapTool, Range1d
+from bokeh.models import FixedTicker, CustomJS, TapTool, Range1d, ColumnDataSource
 from bokeh.palettes import brewer
 
 import study_info as SI
@@ -24,7 +24,7 @@ class avgh1:
 
 		s.samp_freq = 256
 
-	def show_file_heirarchy(s):
+	def show_file_hierarchy(s):
 		def disp_node( name, node):
 		    print(name)
 		    indent = name.count('/')*5*' '
@@ -45,12 +45,10 @@ class avgh1:
 			s.cases = {}
 			s.case_num_map = {}
 			for vals in case_info.value:
-				#print([type(v) for v in vals])
 				dvals = [ v[0].decode() if type(v[0]) == np.bytes_ else v[0] for v in vals   ]
 				caseD = { n:v for n,v in zip(case_info.dtype.names, dvals) }
 				s.cases[ caseD['case_num'] ] = caseD
 				s.case_num_map[ caseD['case_type'] ] = caseD['case_num']
-			#s.case_num_map = { D['case_type']:k for k,D in s.cases.items() }
 
 		else:
 			return
@@ -96,7 +94,7 @@ class avgh1:
 		g=gridplot([plots])
 		show(g)
 
-	def selected_cases_by_channel(s,case_list,props={}):
+	def selected_cases_by_channel(s,case_list,channel_list='all',props={}):
 		default_props = {'width':250,
 						'height':150,
 						'min_border':2,
@@ -119,34 +117,60 @@ class avgh1:
 		else: props['colors'] = ['#2222DD','#DD2222','#66DD66']
 
 		callback = CustomJS( code="alert('clicked')" )
-		#callback = CustomJS( code="function(){ var data = source.get('data'); console.dir(data); }" )
+
+		if channel_list ==  'all':
+			channel_list = s.electrodes
+
+		
+		n_plots = len(channel_list) #potentials.shape[1]
+		n_per_row = int(np.ceil(n_plots**0.5))
 
 		plots = []
-		n_elec = potentials.shape[1]
-		n_per_row = int(np.ceil(n_elec**0.5))
-
-		for eind in range(n_elec):
-			if eind % n_per_row == 0:
+		for plot_ind,electrode in enumerate(channel_list):
+			eind= s.electrodes.index(electrode)
+			if plot_ind % n_per_row == 0:
 				plots.append([])
-			electrode = s.electrodes[eind]
+			
 			tap = TapTool( callback=callback )
 			tools=[tap]
-			if n_elec - eind < n_per_row+1:
+			if n_plots - plot_ind < n_per_row+1:
 				bot_flag = True
 			else:
 				bot_flag = False
-			if eind == 0:
+			if plot_ind == 0:
 				leg_flag = True
 			else:
 				leg_flag = False
 
-			splot = s.make_plot_for_channel(potentials,eind,props,case_list,tools,bot_flag,leg_flag)
+			splot = s.make_plot_for_channel(potentials,eind,props,case_list,tools,
+										bottom_label=bot_flag,legend=leg_flag)
 			plots[-1].append(splot)
 		g=gridplot(plots,border_space=-40)
 		show(g)
 
 
-	def make_plot_for_channel(s,pot,el_ind,props,case_list,tools,bottom_label=False,legend=False):
+	def make_data_source(s,times,potentials,channels='all'):
+		s.extract_case_data()
+
+		source_dict = dict(
+						times=times)
+
+		if channels == 'all':
+			channels = s.electrodes
+
+		for chan in channels:
+			ch_ind = s.electrodes.index(chan)
+			for cs_ind,cs in s.cases.items():
+				source_dict[chan+'_'+cs['case_type'] ] = potentials[cs_ind-1,ch_ind,:]
+
+		source = ColumnDataSource(
+			data = source_dict )
+
+		return source
+
+
+	def make_plot_for_channel(s,pot,el_ind,props,case_list,tools,
+						mode='notebook',bottom_label=False,legend=False):
 
 		if bottom_label:
 			height = props['height']+props['extra_bottom_height']
@@ -155,7 +179,7 @@ class avgh1:
 
 
 		plot = figure(width=props['width'], height=height, 
-			title=s.electrodes[el_ind], tools=tools,
+			title=s.electrodes[el_ind], #tools=tools,
 			min_border=props['min_border'])
 		plot.y_range = Range1d(*props['yrange'])
 		plot.title_text_font_size = '8pt'
@@ -166,7 +190,11 @@ class avgh1:
 			leg = None
 			if legend:
 				leg = case
-			plot.line( x=props['times'], y=pot[case_ind,el_ind,:], color=props['colors'][cs_ind],
+			if mode == 'server':
+				plot.line( x='times', y=electrode+'_'+case, color=props['colors'][cs_ind],
+						line_width=3, line_alpha=0.85, name=case, legend=leg)
+			else: #notebook for now
+				plot.line( x=props['times'], y=pot[case_ind,el_ind,:], color=props['colors'][cs_ind],
 						line_width=3, line_alpha=0.85, name=case, legend=leg)
 
 		if legend:
