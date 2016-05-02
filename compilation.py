5273b7a8e9b5133459ddfbe1392da70557561098
mikejseay <mikejseay@gmail.com>
2016-05-02 16:32:26 -0400

    added two methods to compilation to wrap querying the subjects collection and building a dataframe from a cursor

--- compilation.py
+++ compilation.py
@@ -2,7 +2,6 @@
 '''
 
 import pandas as pd
-
 import organization as O
 
 queries = { 'AA GWAS subjects':{'AAfamGWAS':'x'},
@@ -11,12 +10,33 @@ queries = { 'AA GWAS subjects':{'AAfamGWAS':'x'},
                             {'$in':['a','b','c','d']}},
         }
 
-def join_collection(compDF, collection, name, id_field, df_indices):
+def get_subjectdocs(sample):
+    if sample not in queries.keys():
+        print('sample incorrectly specified, the below are valid')
+        print(queries.keys())
+        return
+    else:
+        docs = O.Mdb['subjects'].find(queries[sample])
+        return docs
+
+def buildframe_fromdocs(docs):
+    df = pd.DataFrame.from_records(list(docs))
+    if 'ID' in df.columns:
+        df.set_index(['ID'], inplace=True)
+    return df
+
+def join_collection(keyDF, coll, name, id_field='ID', join_inds=['ID'],
+    add_query={}):
+
+    query = {id_field:{'$in':list(keyDF.index)}}
+    query.update(add_query)
+
     dDF = pd.DataFrame.from_records([O.flatten_dict(r) for r in \
-            list( collection.find({id_field:{'$in':list(compDF.index)},
-                                  'questname':name}) ) ] )
+            list( coll.find(query) ) ] )
+
     dDF['ID'] = dDF[id_field]
-    dDF.set_index(df_indices,inplace=True)
+    dDF.set_index(join_inds,inplace=True)
     dDF.columns = [ name+'_'+c for c in dDF.columns ]
-    jDF = compDF.join(dDF)
+
+    jDF = keyDF.join(dDF)
     return jDF
